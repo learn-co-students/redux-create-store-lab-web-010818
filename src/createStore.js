81c602e6fc274f2909d4603e8ea38980f6ced9cf
feihafferkamp <fei.hafferkamp@gmail.com>
2018-03-30 16:16:10 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,22 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  // call dispatch and pass in the action {type: '@@INIT'}
+  dispatch({ type: '@@INIT' });
+
+  return {
+    getState,
+    dispatch
+  };
 }
 
 function render() {
